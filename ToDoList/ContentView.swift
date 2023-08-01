76c876e11400c330158bf952348f7289a0286b2a
Hannah Dinh <hannahdinhHD@gamil.com>
2023-08-01 11:33:52 -0700

    Iteration 0 complete

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            HStack{
+                Text("To Do List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                
+                Spacer()
+                
+                Button(action:{}){
+                    Text("+")
+                        .font(.system(size: 40))
+                }
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
